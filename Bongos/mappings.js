3b9facc38ad03265e274cc4ee95879d78abc2344
Rose Marie Tan <rose.marie.tzy@gmail.com>
2012-04-17 21:44:02 +0800

    all files

---
+++ Bongos/mappings.js
@@ -0,0 +1,9 @@
+{
+    "hihat": {"version": 2, "url": "hihat.wav"},
+    "kick": {"version": 2, "url": "kick.wav"},
+    "snare": {"version": 2, "url": "snare.wav"},
+    "tom1": {"version": 2, "url": "tom1.wav"},
+    "tom2": {"version": 2, "url": "tom2.wav"},
+    "tom3": {"version": 2, "url": "tom3.wav"}
+}
+
