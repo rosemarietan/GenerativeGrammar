792d19dab0e37c73e8623cb644be0f74103fd2c7
Rose Marie Tan <rose.marie.tzy@gmail.com>
2012-04-19 20:22:19 +0800

    removed unused folders

--- Bongos/mappings.js
+++
@@ -1,9 +0,0 @@
-{
-    "hihat": {"version": 2, "url": "hihat.wav"},
-    "kick": {"version": 2, "url": "kick.wav"},
-    "snare": {"version": 2, "url": "snare.wav"},
-    "tom1": {"version": 2, "url": "tom1.wav"},
-    "tom2": {"version": 2, "url": "tom2.wav"},
-    "tom3": {"version": 2, "url": "tom3.wav"}
-}
-
